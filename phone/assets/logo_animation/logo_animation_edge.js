05e33a9878dbb0bf146d66b008487e4224560f94
riezbeats <waynemoore22@icloud.com>
2018-09-22 16:24:31 -0400

    change2

---
+++ phone/assets/logo_animation/logo_animation_edge.js
@@ -0,0 +1,9 @@
+
+(function(compId){var _=null,y=true,n=false,rz='rotateZ',x3='5.0.1.386',e7='${logo}',x4='rgba(0,0,0,0)',x1='5.0.1',g='image',x6='rgba(255,0,0,0.00)',x2='5.0.0',i='none';var g5='logo.png';var im='images/',aud='media/',vid='media/',js='js/',fonts={},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=[],symbols={"stage":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:'logo',t:g,r:['0px','0px','100px','100px','auto','auto'],f:[x4,im+g5,'0px','0px'],tf:[[],['360']]}],style:{'${Stage}':{isStage:true,r:[undefined,undefined,'100px','100px'],overflow:'hidden',f:[x6]}}},tt:{d:2000,a:y,data:[["eid1",rz,0,2000,"linear",e7,'0deg','360deg']]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-8195931");
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",2000,function(sym,e){sym.play();});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+})})(AdobeEdge.$,AdobeEdge,"EDGE-8195931");
